4355348b242db11f25cd262dcae38318136e6566
Vuk Aleksandrov <Vuk.Aleksandrov101@gmail.com>
2022-08-10 20:48:27 +0200

    Test Commit

---
+++ build/CMakeFiles/Game.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Game.dir/Game/src/main.cpp.obj"
+  "CMakeFiles/Game.dir/Game/src/main.cpp.obj.d"
+  "Game.exe"
+  "Game.exe.manifest"
+  "Game.pdb"
+  "libGame.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

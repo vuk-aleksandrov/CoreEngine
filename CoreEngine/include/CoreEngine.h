87b9a5ecd1e71289b79af94cb2ac156ad251c3b8
Vuk Aleksandrov <Vuk.Aleksandrov101@gmail.com>
2022-08-11 17:53:21 +0200

    Basic window created

--- CoreEngine/include/CoreEngine.h
+++
@@ -1,7 +0,0 @@
-#include "export.h"
-
-namespace core
-{
-    CORE_API int pow(int x, int y);
-    CORE_API int sq(int x);
-}
--- Game/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <CoreEngine.h>
-
-#include <iostream>
-
-int main()
-{
-    std::cout << core::sq(5) << ' ' << core::pow(5, 3) << std::endl;
-    return 0;
-}
